0f23d5a616f6f04ad58652a64e5c8a520ff89358
MK Son <smg2252@naver.com>
2016-02-21 19:55:53 +0900

    커밋

--- app/src/main/java/com/example/son/testproject1/MainActivity.java
+++ app/src/main/java/com/example/son/testproject1/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.son.testproject1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        new Toast(MainActivity.this).makeText(MainActivity.this,"출력테스트",Toast.LENGTH_SHORT).show();
     }
 }
